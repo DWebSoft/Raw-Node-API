3dde5b849148820c7bfb4e75425f07b56dab2a55
DWebSoft <dwebsoftguy@gmail.com>
2018-04-02 20:02:59 +0530

    Parse Headers, Method, Query Params And Payload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  //Dependecies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 //Create a server
 var server = http.createServer( function (req, res) {
@@ -13,13 +14,35 @@ var server = http.createServer( function (req, res) {
     var parsedUrl = url.parse(req.url, true);
     
     //Get the path
-    var path = parsedUrl.path;
+    var path = parsedUrl.pathname;
     
     //Get the resource name
     var resource = path.replace(/^\/+|\/+$/g,'');
-    console.log(resource);
 
-    res.end('Hello World');
+    //Get query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    //Get Requested Method
+    var method = req.method.toUpperCase();
+
+    //Get Headers
+    var headers = req.headers;
+
+   //Get Payload
+   var payload = '';
+   var decoder = new StringDecoder('utf-8');
+
+   //Payload comes as a Stream. So grab it in parts and combine
+   req.on('data', (data) => {
+        payload += decoder.write(data);
+      
+   });
+
+   req.on('end', () => {
+       payload += decoder.end();
+       console.log('Payload', payload);
+       res.end('Hello World');
+   });
 });
 
 //Listen
