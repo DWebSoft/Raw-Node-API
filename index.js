ce4e6d7f0a5da7326d36e1a0a8828e301318ebee
DWebSoft <dwebsoftguy@gmail.com>
2018-04-08 08:14:44 +0530

    Background Workers Added

--- index.js
+++ index.js
@@ -2,107 +2,25 @@
  * Primary File for API
  */
 
- //Dependecies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
+ //Dependencies
+ var server = require('./lib/server');
+ var workers = require('./lib/workers');
 
-//Instanstiate a http server
-var httpServer = http.createServer( function (req, res) {
-    unifiedServer(req, res);   
-});
+ //Declare the app
+ var app = {};
 
-//Start a http server
-httpServer.listen(config.httpPort, function() {
-    console.log(`Server started listening at port ${config.httpPort} in ${config.envName} environment`);
-})
+ //Init app
+ app.init = function(){
+    //Start Server
+    server.init();
 
-//Instanstiate a https server
-var httpsServerOptions = {
-    key: fs.readFileSync('./https/privatekey.key'),
-    cert: fs.readFileSync('./https/certificate.crt'),
-}
-var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-    unifiedServer(req, res);
-});
+    //Start Workers
+     workers.init();
 
-//Start a https server
-httpsServer.listen(config.httpsPort, function () {
-    console.log(`Server started listening at port ${config.httpsPort} in ${config.envName} environment`);
-})
+ }
 
-//All server logic for both http and https server
-var unifiedServer = function(req, res) {
-    //Parse the incoming url
-    var parsedUrl = url.parse(req.url, true);
+ //Execute
+ app.init();
 
-    //Get the path
-    var path = parsedUrl.pathname;
-
-    //Get the resource name
-    var resource = path.replace(/^\/+|\/+$/g, '');
-
-    //Get query string as an object
-    var queryStringObject = parsedUrl.query;
-
-    //Get Requested Method
-    var method = req.method.toLowerCase();
-
-    //Get Headers
-    var headers = req.headers;
-
-    //Get Payload
-    var buffer = '';
-    var decoder = new StringDecoder('utf-8');
-
-    //Payload comes as a Stream. So grab it in parts and combine
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-
-    });
-
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        //Choose the handler this request should go to
-        var chooseHandler = typeof (router[resource]) !== 'undefined' ? router[resource] : handlers.notFound;
-
-        //Construct the data object to be send to router
-        var data = {
-            resource,
-            queryStringObject,
-            method,
-            headers,
-            payload: helpers.parseJsonToObject(buffer)
-        };
-
-        //Route the request to the specific handler
-        chooseHandler(data, (statusCode, payload) => {
-
-            //Set default status code
-            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
-
-            //Set default payload object
-            payload = typeof (payload) == 'object' ? payload : {};
-
-            var payloadString = JSON.stringify(payload);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-        });
-    });
-}
-
-//Define request router
-var router = {
-    ping : handlers.ping,
-    users : handlers.users,
-    tokens: handlers.tokens,
-    checks: handlers.checks
-}
+ //Export app module
+ module.exports = app;
